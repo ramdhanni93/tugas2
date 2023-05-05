fc480cd93caeda21b801182e1f9e9270542a0834
ramdhanni93 <128136751+ramdhanni93@users.noreply.github.com>
2023-05-05 15:54:11 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan  : Ramdani</li>
+        <li>Nama Belakang   : Dani</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Jalan Percetakan Negara IV No. 10-13</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta</li>
+    </ul>
+
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Berenang</li>
+        <li>Lari</li>
+    </ul>
+</section>
